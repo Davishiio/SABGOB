a866ee5c29c85348719663ba8782943ace1a44cc
Davishiio <dtdavid@gmail.com>
2026-01-12 19:23:14 -0500

    Se añade la migraciòn de la tabla tareas menteniendo la relaciòn correspondiente

---
+++ database/migrations/2026_01_12_230730_create_tareas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            //constrained('proyectos'): Conecta a la tabla 'proyectos'
+            $table->foreignId('idProyecto')->constrained('proyectos')->onDelete('cascade');
+            $table->string('titulo');
+            $table->text('descripcion')->nullable();
+             $table->enum('estado', ['pendiente', 'completado'])->default('pendiente');
+            $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
