8c659f765a33602b93c810ef02a58f8dd9667b9e
Davishiio <dtdavid@gmail.com>
2026-01-12 19:42:08 -0500

    Creaciòn de la migraciòn para las subtareas.

---
+++ database/migrations/2026_01_13_003335_create_subtareas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subtareas', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('idTarea')->constrained('tareas')->onDelete('cascade');
+
+            $table->string('titulo');
+            $table->text('descripcion')->nullable(); // Opcional según tu diagrama
+            $table->enum('estado', ['pendiente', 'completado'])->default('pendiente');
+
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subtareas');
+    }
+};
